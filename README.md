fdf2d03367a1df80056b82286c206bb0d2da3467
curist <curist.cyc@gmail.com>
2014-05-06 15:55:12 +0800

    清code

---
+++ README.md
@@ -0,0 +1,9 @@
+# Install Things
+``` sh
+npm install -g gulp nodemon
+```
+
+# Start Developing
+``` sh
+npm run start-dev
+```
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 var gulp = require('gulp');
-var nodemon = require('gulp-nodemon');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var reactify = require('reactify');
@@ -16,18 +15,6 @@ gulp.task('browserify', function() {
   .pipe(source('bundle.js'))
   .pipe(gulp.dest('public/js'))
   ;
-  /*
-  return gulp.src('app/front_entry.js')
-    .pipe(browserify({
-      'opts.basedir': './',
-      transform: [reactify],
-      extensions: ['.jsx']
-    }))
-    .on('error', function(e) {
-      console.warn(e);
-    })
-    .pipe(gulp.dest('public/js'));
-  */
 });
 
 gulp.task('watch', function() {
@@ -37,20 +24,7 @@ gulp.task('watch', function() {
   ], ['browserify']);
 });
 
-gulp.task('nodemon', function() {
-  nodemon({
-    script: 'server.js',
-    ignore: [],
-    env: { 'NODE_PATH': '.' }
-  }).on('change', function() {
-    // console.log('change');
-  }).on('restart', function() {
-    // console.log('restart');
-  });
-});
-
 gulp.task('default', [
-  'watch',
-  // 'nodemon'
+  'watch'
 ]);
 
