e0a0acfdd84a128a97afb83812a6a5301d5e2acb
curist <curist.cyc@gmail.com>
2014-05-07 12:02:33 +0800

    後端ren完前端ren

--- app/front_entry.js
+++ app/front_entry.js
@@ -1,10 +1,22 @@
-var React = require('react');
-var RootNode = require('app/views/root_node');
+/** @jsx React.DOM */
 
+var React = require('react');
+
+// (function() {
+//   var SiteNode = require('app/views/site_node');
+
+//   console.log('很酷內');
+//   React.renderComponent(
+//     <SiteNode side="client" />,
+//     document
+//   );
+
+// })();
+
+var RootNode = require('app/views/root_node');
 React.renderComponent(
   RootNode({side: 'client'}),
   document.getElementById('container')
 );
-
 
 
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ require('node-jsx').install({
 
 var express = require('express');
 var app = express();
+var url = require('url');
 
 app.use(express.compress());
 app.use(express.static(__dirname + '/public'));
@@ -16,7 +17,13 @@ var template = React.renderComponentToString(SiteNode({
   side: 'server'
 }));
 
-app.get('*', function(req, res) {
+app.get('/', function(req, res) {
+  res.redirect('/p/');
+});
+
+app.get('/p/*', function(req, res) {
+  var req_path = url.parse(req.url).pathname;
+  console.log(req_path);
   res.end(template);
 });
 
