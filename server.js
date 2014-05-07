45e7d8b1797b5b4b80ccf17d58fc0bf2c6ad1bd4
curist <curist.cyc@gmail.com>
2014-05-07 13:38:26 +0800

    prepend '/p/' for react routing

--- server.js
+++ server.js
@@ -13,17 +13,20 @@ app.use(express.static(__dirname + '/public'));
 var React = require('react');
 var SiteNode = require('app/views/site_node');
 
-var template = React.renderComponentToString(SiteNode({
-  side: 'server'
-}));
-
+// TODO '/p' should also redirect to '/p/'
 app.get('/', function(req, res) {
   res.redirect('/p/');
 });
 
 app.get('/p/*', function(req, res) {
   var req_path = url.parse(req.url).pathname;
-  console.log(req_path);
+  var route_path = req_path.replace(/^\/p/, '');
+
+  var template = React.renderComponentToString(SiteNode({
+    side: 'server',
+    route_path: route_path
+  }));
+
   res.end(template);
 });
 
