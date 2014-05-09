5a5cc30cec754aac7630d6b41f9e549857b10fec
curist <curist.cyc@gmail.com>
2014-05-09 12:29:06 +0800

    react-async

--- app/views/site_node.jsx
+++ app/views/site_node.jsx
@@ -1,11 +1,16 @@
 /** @jsx React.DOM */
 
 var React = require('react');
+var ReactAsync = require('react-async');
 
 var SiteBoilerPlate = require('app/views/_core/SiteBoilerPlate');
 var RootNode = require('app/views/root_node');
 
 module.exports = React.createClass({
+  mixins: [ReactAsync.Mixin],
+  getInitialStateAsync: function(cb) {
+    cb(null, {});
+  },
   render: function() {
     return (
       <SiteBoilerPlate>
--- server.js
+++ server.js
@@ -15,9 +15,18 @@ app.use(express.compress());
 app.use(express.static(__dirname + '/public'));
 
 
-var React = require('react');
-var SiteNode = require('app/views/site_node');
+var request = require('request');
+app.get('/api/news', function(req, res) {
+  request('http://api.ihackernews.com/page', function(err, resp, body) {
+    console.log(body);
+    res.json(JSON.parse(body));
+  });
+});
 
+
+var React = require('react');
+var ReactAsync = require('react-async');
+var SiteNode = require('app/views/site_node');
 app.get('/*', function(req, res) {
   var routing = require('app/routing');
   var router = routing.getRouter();
@@ -37,12 +46,14 @@ app.get('/*', function(req, res) {
     on: function() {
       this.deferred.resolve();
       // TODO 準備好properties
+      // TODO 處理錯誤，promise被reject時...?
       this.promise.then(function(properties) {
-        var template = React.renderComponentToString(SiteNode({
+        ReactAsync.renderComponentToStringWithAsyncState(SiteNode({
           side: 'server',
           route: pathname
-        }));
-        res.end(template);
+        }), function(err, template) {
+          res.end(template);
+        });
       });
     }
   });
